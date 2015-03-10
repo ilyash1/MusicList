fee961a56eebb1bfbac4117f911c90a9d805c6c3
ilyash1 <hira_245@hotmail.com>
2015-03-10 10:06:54 -0400

    Part 1 DONE
    DONE WITH PART 1

--- MusicRunner.java
+++ MusicRunner.java
@@ -1,16 +1,22 @@
-import java.util.Arrays;
+import java.util.*;
  
 public class MusicRunner
 {
   public static String Clean (String str)
   {
-    return str.substring(1, str.length()-1).trim();
+    String output = "";
+    
+    if (str.length() > 2)
+      output = str.substring(1, str.length()-1).trim();
+    
+    return output;
   }
   
   public static void main (String[] args)
   {
     int count = 0;
     MusicReader mr = new MusicReader();
+    ArrayList<Song> songs = new ArrayList<Song>();
     
     
     mr.open("musiclist.csv");
@@ -32,30 +38,60 @@ public class MusicRunner
      // System.out.println(Arrays.toString(data));
       
       // Let's try to create a Song object
-      int year = Integer.parseInt(Clean(data[3]));
-      double score = Double.parseDouble(Clean(data[4]));
+      int year = 0;
+      double score = 0;
+      
+      try
+      {
+           year = Integer.parseInt(Clean(data[3]));
+           score = Double.parseDouble(Clean(data[4]));
+      }
+      catch (Exception e)
+      {
+        
+      }
       
       Song song = new Song(Clean(data[0]), Clean(data[1]), year, score,Clean(data[16]));  // data[0] is the artist and data[1] is the name
       
       if (Clean(data[2]).equals("song"))
       {
         // Add song object to ArrayList
+        if (song.name.indexOf("love") != -1 && song.year > 2000)
+        {
+          songs.add(song);
+        
+          count++;
+        }
       }
       
+      /*
       System.out.print(song.artist + ", ");
       System.out.print(song.name + ", ");
       System.out.print(song.year + ", ");
       System.out.print(song.score + ", ");
       System.out.println(song.notes);
       
-      count++;
+      */
       
-      if (count == 10)  // For now only read ONE song
+      if (count == 10000)  // For now only read ONE song
         break;
       
       data = mr.getSongData();  // Get next line of song data
     }
     
     mr.close();
+    
+    
+    // Loop through the songs list and print out all the song data
+    for (int i = songs.size()-1; i >= 0; i--)
+    {
+      Song song = songs.get(i);
+      
+      System.out.print(song.artist + ", ");
+      System.out.print(song.name + ", ");
+      System.out.print(song.year + ", ");
+      System.out.print(song.score + ", ");
+      System.out.println(song.notes);      
+    }
   }
 }
